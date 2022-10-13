388fe51e521a3fe6f1468a83a0b7529524587a57
Aleksey <J2k_pot@mail.ru>
2022-10-13 17:38:42 +0300

    add_68

---
+++ Task_68/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Введите число M:  ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N:  ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int akkermanFuncion = AkkermanFuncion(m, n);
+Console.WriteLine(akkermanFuncion);
+int AkkermanFuncion(int m, int n)
+
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    if (m > 0 && n == 0)
+    {
+        return AkkermanFuncion(m - 1, 1);
+    }
+    if (m > 0 && n > 0)
+    {
+        return AkkermanFuncion(m - 1, AkkermanFuncion(m, n - 1));
+    }
+    return 0;
+}
